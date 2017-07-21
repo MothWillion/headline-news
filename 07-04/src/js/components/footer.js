d90d64c013d88fb4048554157dece1dbb3a6f127
sunjun <1428312034@qq.com>
2017-07-21 17:06:03 +0800

    JSX内置表达式

---
+++ 07-04/src/js/components/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Footer extends React.Component{
+  render(){
+    return(
+      <footer>
+        <h1>这是底部</h1>
+      </footer>
+    )
+  }
+}
+
+export default Footer;
