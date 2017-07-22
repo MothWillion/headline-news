8e781ea2b50353245850c62aa6c1e605517c64cb
sunjun <1428312034@qq.com>
2017-07-22 08:20:22 +0800

    props属性

---
+++ 08-02/src/js/components/body.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Body extends React.Component{
+  constructor(){
+    super();
+  }
+  render(){
+    return(
+      <div>
+        <h1>这是页面主题</h1>
+        <input type="button" value="按钮" disabled={false} onClick={this.props.console}/>
+        {/*注释*/}
+        <div>{this.props.userId}</div>
+      </div>
+    )
+  }
+}
+
+export default Body;
